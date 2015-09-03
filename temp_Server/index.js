7979b92575cb8f585581c93944a5a2c79a3b9ac8
Jonathan Lake <jwtlake@gmail.com>
2015-09-02 21:24:23 -0700

    updated api for reader updates

--- temp_Server/index.js
+++ temp_Server/index.js
@@ -1,54 +1,27 @@
-var Hapi = require('hapi');  //framework
-var pg = require('pg'); //postgres libary
-
-var dbInfo = require('./dbInfo'); //private conection screens
+var Hapi = require('hapi');
+var ip = require('ip'); 
 
 //create new server object
+var port = 3000;
 var server = new Hapi.Server();
-server.connection({ port: 3000});
-
-console.log('Connecting to db');
-
-//connect to postgres server
-var query = 'select s.name, st.type from source as s join sourcetype as st on s.sourcetypeid = st.id;';
-
-pg.connect(dbInfo.conString, function(err, client, done) {
-	if(err) {
-		return console.error('error fetching client from pool', err);
-	}
-	
-	client.query(query, function(err, result) {
-		//call `done()` to release the client back to the pool
-		done();
-
-		if(err) {
-			return console.error('error running query', err);
-		}
-		console.log(result.rows[0]);
-    		//output: 1
-	});
+server.connection({
+    port: port
 });
 
-//route for displaying current temp
-server.route({
-	path: '/temp/current',
-	method: ['GET','POST']
-	handler: function(request, reply) {
-		if(request.method === 'get') {
-			reply('Its Hot');
-		} else {
-			reply.redirect('/');
-		}
-	}
-});
-
-//look for onRequest event, call back function
-server.ext('onRequest', function(request, reply) {
-	console.log('Request received: ' + request.path);
-	reply.continue(); //continue request life cycle
-});
+//routes
+server.route(require('./routes/index.js'));
 
 //start server
 server.start(function() {
-	console.log('Listening in ' + server.info.uri);
+	console.log('Webserver Started @' + ip.address() + ':' + port + ' Listening...');
 });
+
+//debug
+//var readingController = require('./controllers/readingController.js');
+//var readingObject = require('./objects/readingObject.js');
+//var controller = new readingController();
+//var reading = new readingObject(null,100.02,1,'2011-05-16 15:36:38',null);
+
+//controller.newReading(reading);
+//controller.getAllReadings();
+
